2e4c1fc105df1e80603ee668cf3977f183f50f4d
Siva Ganesh <sivaganeshv1729@gmail.com>
2026-06-03 22:07:17 +0530

    chore: remove original spring boot and vite codebases

--- frontend/src/components/ui/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Spinner = () => (
-  <div className="flex justify-center items-center p-8">
-    <div className="w-8 h-8 border-4 border-primary/20 border-t-primary rounded-full animate-spin"></div>
-  </div>
-);
-
-export default Spinner;
